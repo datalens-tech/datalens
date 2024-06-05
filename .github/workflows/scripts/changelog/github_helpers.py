42f2f009e12e419080c517ed213e0404c9689860
KonstantAnxiety <58992437+KonstantAnxiety@users.noreply.github.com>
2024-06-05 13:29:58 +0300

    Better retries for GitHub API requests (#149)
    * Better retries when requesting GitHub API
    
    * Ratelimit reset
    
    * wording
    
    * fix

--- .github/workflows/scripts/changelog/github_helpers.py
+++ .github/workflows/scripts/changelog/github_helpers.py
@@ -56,17 +56,39 @@ def get_latest_repo_release(repo_full_name: str, headers: dict[str, str]) -> str
 
 def request_with_retries(
     req_func: Callable[[], requests.Response],
-    max_retries: int = 15,
+    max_retries: int = 20,
     retry_delay: int = 10,
 ) -> requests.Response:
+
+    def _get_retry_delay(response: requests.Response) -> Optional[int]:
+        if response.status_code == 403 and "X-RateLimit-Remaining" not in resp.headers or response.status_code >= 500:
+            return retry_delay
+
+        if response.status_code in (403, 429):
+            if int(resp.headers.get("X-RateLimit-Remaining", -1)) > 0:
+                return retry_delay
+
+            ratelimit_reset = int(resp.headers.get("X-RateLimit-Reset", -1))
+            if ratelimit_reset > 0:
+                current_timestamp = int(datetime.datetime.now().timestamp())
+                return ratelimit_reset - current_timestamp + 1
+
+        return None
+
     retries = 0
     while retries < max_retries:
         resp = req_func()
-
-        if resp.status_code in (403, 429) and int(resp.headers.get("X-RateLimit-Remaining", -1)) == 0 or resp.status_code >= 500:
-            LOGGER.info(f"Got status {resp.status_code} on try {retries + 1}, going to retry in {retry_delay}s...")
-            retries += 1
-            time.sleep(retry_delay)
+        if resp.status_code != 200:
+            delay = _get_retry_delay(resp)
+            if delay is not None:
+                LOGGER.info(f"Got status {resp.status_code} on try {retries + 1}, going to retry in {delay}s...")
+                retries += 1
+                if retries >= max_retries:
+                    resp.raise_for_status()
+                    raise RuntimeError(f"Reached maximum number of retries, last response: {resp}")
+                time.sleep(delay)
+            else:
+                resp.raise_for_status()
         else:
             return resp
 
