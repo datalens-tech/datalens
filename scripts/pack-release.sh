ade80cdcdca169cf505318585b0d74dea8532b6c
Georgy Malkov <malkovgv@yandex-team.ru>
2025-04-16 14:30:10 +0300

    Support split save images (#305)
    * Add scripts to pack release to single archive with split images
    
    * fix load image
    
    * fix pack image
    
    * fix
    
    * add pack release

--- scripts/pack-release.sh
+++ scripts/pack-release.sh
@@ -13,6 +13,7 @@ export COPYFILE_DISABLE=1
 
 IMAGE_PLATFORM="linux/amd64"
 README_URL=""
+SPLIT_MODE="false"
 
 # parse args
 for _ in "$@"; do
@@ -27,6 +28,10 @@ for _ in "$@"; do
     shift # past argument
     shift # past value
     ;;
+  --split)
+    SPLIT_MODE="true"
+    shift # past argument with no value
+    ;;
   -*)
     echo "unknown arg: ${1}"
     exit 1
@@ -39,28 +44,33 @@ echo ""
 echo "Start pack all release to single archive..."
 echo "  platform: ${IMAGE_PLATFORM}"
 
-"${SCRIPT_DIR}/save-images.sh" --platform "${IMAGE_PLATFORM}" --force
-
-echo ""
-echo "Continue pack release..."
-
 VERSION=$(jq -r '.releaseVersion' "${SCRIPT_DIR}/../versions-config.json")
 echo "  version: ${VERSION}"
 
 FILE_RELEASE="datalens-${VERSION}.tar"
 OUT_PATH="${SCRIPT_DIR}/../dist"
 
+rm -rf "${OUT_PATH}/datalens-${VERSION}"
+rm -rf "${OUT_PATH}/${FILE_RELEASE}.tmp"
+mkdir -p "${OUT_PATH}/datalens-${VERSION}"
+
+if [ "${SPLIT_MODE}" == "true" ]; then
+  echo "  split mode: true"
+  "${SCRIPT_DIR}/save-images.sh" --platform "${IMAGE_PLATFORM}" --force --split --target-dir "${OUT_PATH}/datalens-${VERSION}"
+else
+  echo "  split mode: false"
+  "${SCRIPT_DIR}/save-images.sh" --platform "${IMAGE_PLATFORM}" --force --target-dir "${OUT_PATH}/datalens-${VERSION}"
+fi
+
+echo ""
+echo "Continue pack release..."
 echo "  file: ${FILE_RELEASE}"
 
 echo ""
 echo "Copy resources to [dist/datalens-${VERSION}] directory..."
 
-rm -rf "${OUT_PATH}/datalens-${VERSION}"
-rm -rf "${OUT_PATH}/${FILE_RELEASE}.tmp"
-mkdir -p "${OUT_PATH}/datalens-${VERSION}"
-
-cp "${SCRIPT_DIR}/../datalens-images.tar.gz" "${OUT_PATH}/datalens-${VERSION}/datalens-images.tar.gz"
 cp "${SCRIPT_DIR}/../README.md" "${OUT_PATH}/datalens-${VERSION}/README.md"
+cp "${SCRIPT_DIR}/../docker-compose.yaml" "${OUT_PATH}/datalens-${VERSION}/docker-compose.yaml"
 cp "${SCRIPT_DIR}/save-images.sh" "${OUT_PATH}/datalens-${VERSION}/save-images.sh"
 cp "${SCRIPT_DIR}/load-images.sh" "${OUT_PATH}/datalens-${VERSION}/load-images.sh"
 cp "${SCRIPT_DIR}/dump-entries.sh" "${OUT_PATH}/datalens-${VERSION}/dump-entries.sh"
@@ -82,7 +92,7 @@ echo ""
 echo "Pack release to [dist/datalens-${VERSION}/${FILE_RELEASE}] file..."
 
 # shellcheck disable=SC2035
-cd "${OUT_PATH}" && tar --no-xattrs -cvf "${OUT_PATH}/${FILE_RELEASE}.tmp" * &>/dev/null && mv "${OUT_PATH}/${FILE_RELEASE}.tmp" "${OUT_PATH}/datalens-${VERSION}/${FILE_RELEASE}"
+cd "${OUT_PATH}/datalens-${VERSION}" && tar --no-xattrs -cvf "${OUT_PATH}/${FILE_RELEASE}.tmp" * &>/dev/null && mv "${OUT_PATH}/${FILE_RELEASE}.tmp" "${OUT_PATH}/datalens-${VERSION}/${FILE_RELEASE}"
 
 popd &>/dev/null
 
