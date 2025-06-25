ef78224d58b6e6aa7ccc60c268c3ada24cacc49b
Georgy Malkov <malkovgv@yandex-team.ru>
2025-06-25 11:25:26 +0300

    Fix tofu lock file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,11 +7,11 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.36.0"
+      version = "2.36.0" # 26.02.2025
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.17.0"
+      version = "2.17.0" # 20.12.2024
     }
   }
 
