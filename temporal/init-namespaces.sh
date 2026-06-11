29db4df04c60d242756be20c0a8c78d4c907b2dd
Sergei Borodin <seray@yandex-team.ru>
2026-06-11 11:03:11 +0200

    feat: enhance init script to manage tempora l search attributes (#425)
    * feat: enhance init script to manage temporal search attributes
    
    * feat: update temporal version suffix to 0.3.0

--- temporal/init-namespaces.sh
+++ temporal/init-namespaces.sh
@@ -35,3 +35,37 @@ for NS in "${INIT_NAMESPACES[@]}"; do
 done
 
 echo "  [temporal-init-namespaces] finish init temporal namespaces..."
+
+# Search attributes are passed via SEARCH_ATTRIBUTES as a comma-separated list
+# of entries in the [namespace:name:type] format
+echo "  [temporal-init-namespaces] start init temporal search attributes..."
+
+IFS=',' read -ra INIT_SEARCH_ATTRIBUTES <<<"${SEARCH_ATTRIBUTES}"
+
+for SA in "${INIT_SEARCH_ATTRIBUTES[@]}"; do
+  [ -z "${SA}" ] && continue
+
+  IFS=':' read -r SA_NAMESPACE SA_NAME SA_TYPE <<<"${SA}"
+
+  if [ -z "${SA_NAMESPACE}" ] || [ -z "${SA_NAME}" ] || [ -z "${SA_TYPE}" ]; then
+    echo "  [temporal-init-namespaces] invalid search attribute [${SA}], expected format [namespace:name:type], skip..."
+    continue
+  fi
+
+  echo "  [temporal-init-namespaces] ensuring search attribute [${SA_NAME}] (${SA_TYPE}) in namespace [${SA_NAMESPACE}]..."
+
+  if ! temporal operator namespace describe "${SA_NAMESPACE}"; then
+    echo "  [temporal-init-namespaces] namespace [${SA_NAMESPACE}] does not exist, skip search attribute [${SA_NAME}]..."
+    continue
+  fi
+
+  if temporal operator search-attribute list --namespace "${SA_NAMESPACE}" | grep -q "${SA_NAME}"; then
+    echo "  [temporal-init-namespaces] search attribute [${SA_NAME}] already exists in [${SA_NAMESPACE}], skip..."
+  else
+    echo "  [temporal-init-namespaces] create search attribute [${SA_NAME}] in [${SA_NAMESPACE}]..."
+    temporal operator search-attribute create --namespace "${SA_NAMESPACE}" --name "${SA_NAME}" --type "${SA_TYPE}"
+    echo "  [temporal-init-namespaces] search attribute [${SA_NAME}] in [${SA_NAMESPACE}] creation complete..."
+  fi
+done
+
+echo "  [temporal-init-namespaces] finish init temporal search attributes..."
